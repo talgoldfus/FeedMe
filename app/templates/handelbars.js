09a35002578141732376af51e44ca3439d529be2
Tal Goldfus <talgoldfus@gmail.com>
2016-08-01 20:06:29 -0400

    Add some bootstrap

--- app/templates/handelbars.js
+++ app/templates/handelbars.js
@@ -14,17 +14,21 @@ function cooktimeTemplate(){
 
 function profileTemplate(){
 template = `
-  <div id="display-profile" class="row">
-    <p>Here are your recipes! </p>
+  <div id="display-profile" class="row text-center">
+    <h1>Here are your recipes! </h1>
       {{#each recipeProfiles}}
-      <div id={{api_id}} class="col-md-4">
-        <h4>{{recipeName}}</h4>
-        <img src={{profile_image}}>
-        <h5>{{sourceDisplayName}}</h5>
-        <h6>{{ingredients}}</h6>
-        <br>
-        <a onclick="$.fn.fullpage.moveTo(4,'slide{{api_id}}');" data-recipeid={{api_id}}>See full details</a>
-      </div>
+
+        <div id={{api_id}} class="col-md-4 text-center">
+          <div class="thumbnail">
+          <img src={{profile_image}}>
+          <div class="caption  text-left">
+          <h4>{{recipeName}}</h4>
+          <h5>{{sourceDisplayName}}</h5>
+          <h6>{{ingredients}}</h6>
+          <p><a class="btn btn-default" onclick="$.fn.fullpage.moveTo(4,'slide{{api_id}}');" data-recipeid={{api_id}}>See full details</a></p>
+          </div>
+        </div>
+        </div>
       {{/each}}
   </div>
     `
@@ -37,14 +41,13 @@ return template
 function fullDetailsTemplate(){
 
   template = `
-
-  <div class="slide" data-anchor="slide{{recipeDetail.apiId}}">
-      <div>
-        <h2>{{recipeDetail.name}} </h2>
+  <div>
+  <div class="slide jumbotron text-center" data-anchor="slide{{recipeDetail.apiId}}">
         <img src = {{recipeDetail.image}}>
-        <a href="{{recipeDetail.source.sourceRecipeUrl}}"> <p> See Full Recipe </p> </a>
-      </div>
-    </div>
+          <h2>{{recipeDetail.name}} </h2>
+          <a class="btn btn-default" href="{{recipeDetail.source.sourceRecipeUrl}}"> <p> See Full Recipe </p> </a>
+  </div>
+  </div>
 
       `
   return template
