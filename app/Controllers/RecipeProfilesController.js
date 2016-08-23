dc338276b53bb4fc49522864973c4e4d4438174e
Tal Goldfus <talgoldfus@gmail.com>
2016-08-23 14:39:01 -0400

    Add comments that better explain the workflow of the app

--- app/Controllers/RecipeProfilesController.js
+++ app/Controllers/RecipeProfilesController.js
@@ -1,20 +1,43 @@
 function searchRecipes() {
     store.profileSearch = []
     store.recipeProfiles = []
+    //Grab information from the API via the adapter functions
     populateSearch().then(
         () => {
             $('#searchedIngredients').val("")
+            //Render the cooking times view and moves down to the appropriate ancor.
             showTimes()
             $.fn.fullpage.moveTo(2);
         }
     )
 }
 
+function createProfiles() {
+    store.profileSearch.forEach((element) => {
+        if (element.totalTimeInSeconds) {
+            new RecipeProfile(element.recipeName, element.flavors, element.ingredients, element.id, element.sourceDisplayName, element.totalTimeInSeconds, element.smallImageUrls[0])
+        }
+    })
+}
+
+function showTimes() {
+    var source = cooktimeTemplate();
+    var template = Handlebars.compile(source)
+    var context = {
+        recipeProfiles: RecipeProfile.cookingTimesString()
+    }
+    var html = template(context)
+    $('#displayTimes').empty()
+    $('#displayTimes').append(html)
+}
+
 function showProfiles(element) {
-    //clear the full detail slides section
+    //clears the full detail slides section
     $(`#displayRecipe`).nextAll().remove()
 
+    //Filters the recipes acording to the selected time frame by the user via the Recipe Profile class method
     let collection = RecipeProfile.filterByRange(parseInt(element.dataset["cookingtime"]), 6)
+
 
     var getRecipeAndBindItToPromise = function createPromisses(recipe){
       return new Promise((resolve) =>{
@@ -36,49 +59,4 @@ function showProfiles(element) {
       loadfullPage()
       $.fn.fullpage.moveTo(3)
   })
-}
-
-// function showProfiles(element) {
-//     let collection = RecipeProfile.filterByRange(parseInt(element.dataset["cookingtime"]), 6)
-//     // Promise.all([getFullDetails(collection[0].api_id),getFullDetails(collection[1].api_id),getFullDetails(collection[2].api_id)])
-//     collection.forEach((recipe) => {
-//         getFullDetails(recipe.api_id)
-//     })
-//
-//
-//     var source = profileTemplate();
-//     var template = Handlebars.compile(source)
-//     var context = {
-//         recipeProfiles: RecipeProfile.filterByRange(parseInt(element.dataset["cookingtime"]), 6)
-//     }
-//     var html = template(context)
-//     $('#displayProfiles').empty()
-//     $('#displayProfiles').append(html)
-//
-//     setTimeout(
-//         function() {
-//             $.fn.fullpage.destroy('all')
-//             loadfullPage()
-//             $.fn.fullpage.moveTo(3);
-//         }, 320);
-// }
-
-
-function createProfiles() {
-    store.profileSearch.forEach((element) => {
-        if (element.totalTimeInSeconds) {
-            new RecipeProfile(element.recipeName, element.flavors, element.ingredients, element.id, element.sourceDisplayName, element.totalTimeInSeconds, element.smallImageUrls[0])
-        }
-    })
-}
-
-function showTimes() {
-    var source = cooktimeTemplate();
-    var template = Handlebars.compile(source)
-    var context = {
-        recipeProfiles: RecipeProfile.cookingTimesString()
-    }
-    var html = template(context)
-    $('#displayTimes').empty()
-    $('#displayTimes').append(html)
 }
